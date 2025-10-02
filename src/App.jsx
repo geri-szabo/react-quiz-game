3f86d581a186936117e7ddbface91531b475bcd9
Szabó Gergő <szabogergo3751@gmail.com>
2025-10-02 21:51:35 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen min-w-screen bg-gray-300">
+      <h1 className='text-8xl text-blue-500'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
